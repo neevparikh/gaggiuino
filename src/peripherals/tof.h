4ef50375d3a9ce5fed6ae0b8645e26ad92dc9b02
Neev Parikh <>
2023-07-29 13:08:43 -0700

    Updated range to reflect ss tank

--- src/peripherals/tof.h
+++ src/peripherals/tof.h
@@ -44,7 +44,7 @@ uint16_t TOF::readLvl() {
 
 uint16_t TOF::readRangeToPct(uint16_t val) {
   static const std::array<uint16_t, 10> water_lvl = { 100u, 90u, 80u, 70u, 60u, 50u, 40u, 30u, 20u, 10u };
-  static const std::array<uint16_t, 9> ranges = { 15u, 30u, 45u, 60u, 75u, 90u, 105u, 115u, 125u };
+  static const std::array<uint16_t, 9> ranges = { 15u, 35u, 55u, 75u, 95u, 115u, 135u, 155u, 175u };
   for (size_t i = 0; i < ranges.size(); i++) {
     if (val <= ranges[i]) {
       return water_lvl[i];
